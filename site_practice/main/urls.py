3a700eabcd5ec74c8767d261759a367b7dc0c393
I-Trush <gari-man@yandex.ru>
2022-11-08 21:56:02 +0300

    32.2 login/logout/user profile

--- site_practice/main/urls.py
+++ site_practice/main/urls.py
@@ -6,4 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
     path('<str:page>/', other_page, name='other'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
 ]
--- site_practice/main/views.py
+++ site_practice/main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -14,3 +17,18 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
+
+
+@login_required()   # 32.2.2. доступ только авторизованным пользователям
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):     # LoginRequiredMixin примесь делающая контроллер класс доступным только авторизованным пользователям
+    template_name = 'main/logout.html'
+
+
