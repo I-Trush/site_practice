f3e279bb803221ff9c14244682f3c6e08aaa000a
I-Trush <gari-man@yandex.ru>
2022-12-01 15:32:03 +0300

    35.4 send new comment notification

--- site_practice/main/models.py
+++ site_practice/main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .utilities import get_timestamp_path
+from .utilities import get_timestamp_path, send_new_comment_notification
+from django.db.models.signals import post_save
+
 
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
@@ -100,4 +102,11 @@ class Comment(models.Model):
         verbose_name = 'Комментарий'
         ordering = ['created_at']
 
+
+def post_save_dispatcher(sender, **kwargs):
+    author = kwargs['instance'].bb.author
+    if kwargs['created'] and author.send_messages:
+        send_new_comment_notification(kwargs['instance'])
+
+post_save.connect(post_save_dispatcher, sender=Comment)
 
--- site_practice/main/utilities.py
+++ site_practice/main/utilities.py
@@ -16,9 +16,22 @@ def send_activation_notification(user):
     user.email_user(subject, body_text)
 
 
-#  34.2 ===========================================================================================
+# 34.2 ===========================================================================================
 from datetime import datetime
 from os.path import splitext
 
 def get_timestamp_path(instance, filename):
     return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
+
+
+# 35.4 ===========================================================================================
+def send_new_comment_notification(comment):
+    if ALLOWED_HOSTS:
+        host = 'http://' + ALLOWED_HOSTS[0]
+    else:
+        host = 'http://localhost:8000'
+    author = comment.bb.author
+    context = {'author': author, 'host': host, 'comment': comment}
+    subject = render_to_string('email/new_comment_letter_subject.txt', context)
+    body_text = render_to_string('email/new_comment_letter_body.txt', context)
+    author.email_user(subject, body_text)
