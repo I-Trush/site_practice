da1ee229ec151b6125f4a5bdced34b3d45490126
I-Trush <gari-man@yandex.ru>
2022-12-06 15:09:30 +0300

    36.1 REST api

---
+++ site_practice/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from main.models import Bb
+
+
+class BbSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at')
+
+
